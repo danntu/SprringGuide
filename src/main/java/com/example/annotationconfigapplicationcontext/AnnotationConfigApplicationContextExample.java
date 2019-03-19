a0f15e4723eb37ff20edec39f422bfec4b18eb29
danik <dan_ntu@mail.ru>
2019-03-19 12:03:14 +0600

    Example of AnnotationConfigApplicationContext in Spring

--- src/main/java/com/example/annotationconfigapplicationcontext/AnnotationConfigApplicationContextExample.java
+++ src/main/java/com/example/annotationconfigapplicationcontext/AnnotationConfigApplicationContextExample.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.annotationconfigapplicationcontext;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
--- src/main/java/com/example/annotationconfigapplicationcontext/Entitlement.java
+++ src/main/java/com/example/annotationconfigapplicationcontext/Entitlement.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.annotationconfigapplicationcontext;
 
 import lombok.Getter;
 import lombok.Setter;
